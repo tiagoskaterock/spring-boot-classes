fabdb496f17cdbe508e77f9e8a08ab5c17e09e37
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-29 15:33:35 -0300

    415. Importando Projeto Maven

---
+++ exercicios-spring-boot/src/test/java/tiago/lemes/exercicios_spring_boot/ExerciciosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tiago.lemes.exercicios_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
