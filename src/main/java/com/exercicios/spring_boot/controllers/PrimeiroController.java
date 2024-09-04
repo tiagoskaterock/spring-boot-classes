bd1abd5c28d67d92f4b2dac203e29a3ad5b74e62
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-09-04 11:10:50 -0300

    Aula 419. Primeiro Web Service ✨

--- src/main/java/com/exercicios/spring_boot/controllers/PrimeiroController.java
+++ src/main/java/com/exercicios/spring_boot/controllers/PrimeiroController.java
@@ -1,5 +1,6 @@
 package com.exercicios.spring_boot.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PrimeiroController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, path = "/ola")
     public String ola() {
-        return "Olá Spring Boot!";
+        return "<h1>Olá Spring Boot!</h1>";
+    }
+
+    @GetMapping
+    public String hello() {
+        return "<h1>Hello</h1>";
+    }
+
+    @GetMapping(path = "helloBananas")
+    public String helloBananas() {
+        return "<h1>Hello Bananas!</h1>";
+    }
+
+    @GetMapping(path = {"/metallica", "/megadeth", "/slayer", "/sepultura", "kreator"})
+    public String bands() {
+        return "<h1>Metallica, Megadeth, Slayer, Kreator, Sepultura!</h1>";
     }
 }
