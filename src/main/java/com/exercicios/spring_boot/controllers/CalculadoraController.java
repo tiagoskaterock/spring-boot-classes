20d58652bc43a7f79cd28bc7f9ade55b0bfcefc4
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-09-14 03:11:18 -0300

    class: 428. Desafio Web Service Calculadora

---
+++ src/main/java/com/exercicios/spring_boot/controllers/CalculadoraController.java
@@ -0,0 +1,21 @@
+package com.exercicios.spring_boot.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/calculadora")
+public class CalculadoraController {
+
+    @GetMapping(path = "/somar/{num1}/{num2}")
+    public int Calculadora(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + num2;
+    }
+
+    @GetMapping(path = "/subtrair")
+    public int subtrair(
+            @RequestParam(name = "num1") int num1,
+            @RequestParam(name = "num2") int num2
+    ) {
+        return num1 - num2;
+    }
+}
