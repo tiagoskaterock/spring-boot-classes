278c7fe07c06ef67d0a6ada9071debd1bdaa3094
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-10-02 14:26:49 -0300

    class: 439. Desafio Novo Atributos - Resposta

--- src/main/java/com/exercicios/spring_boot/controllers/ProdutoController.java
+++ src/main/java/com/exercicios/spring_boot/controllers/ProdutoController.java
@@ -16,8 +16,8 @@ public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
     @PostMapping
-    public Produto novoProduto(@RequestParam String nome) {
-        Produto produto = new Produto(nome);
+    public Produto novoProduto(@RequestParam String nome, @RequestParam float preco, @RequestParam float desconto) {
+        Produto produto = new Produto(nome, preco, desconto);
         produtoRepository.save(produto);
         return produto;
     }
--- src/main/java/com/exercicios/spring_boot/model/entities/Produto.java
+++ src/main/java/com/exercicios/spring_boot/model/entities/Produto.java
@@ -2,7 +2,6 @@ package com.exercicios.spring_boot.model.entities;
 
 import com.exercicios.spring_boot.model.entities.repositories.ProdutoRepository;
 import jakarta.persistence.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Entity
@@ -12,18 +11,35 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Autowired
-    @Transient
-    public ProdutoRepository repository;
+    private String nome;
+
+    public float getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(float desconto) {
+        this.desconto = desconto;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    private float desconto;
+    private float preco;
 
     public Produto() {
     }
 
-    public Produto(String nome) {
+    public Produto(String nome, float preco, float desconto) {
         this.nome = nome;
+        this.desconto = desconto;
+        this.preco = preco;
     }
-
-    private String nome;
 
     public int getId() {
         return id;
